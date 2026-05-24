4695de752892081271672e65fa7f4dc98d064bd9
aracode-programming <aracode.programming@gmail.com>
2026-05-24 19:39:12 +0900

    Initial clean commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+<<<<<<< HEAD
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
+=======
+// https://astro.build/config
+export default defineConfig({});
+>>>>>>> d0673803388ed0abc722dfecdfd3458272cc143f
