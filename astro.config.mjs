9716502041da0d4e79a7db3ddd518c1a304d50f5
aracode-programming <aracode.programming@gmail.com>
2026-05-24 19:54:56 +0900

    パッケージ更新とAstro設定のエラー修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-<<<<<<< HEAD
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
@@ -10,7 +8,3 @@ export default defineConfig({
     plugins: [tailwindcss()]
   }
 });
-=======
-// https://astro.build/config
-export default defineConfig({});
->>>>>>> d0673803388ed0abc722dfecdfd3458272cc143f
